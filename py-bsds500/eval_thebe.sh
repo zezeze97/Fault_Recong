06124a72f2f19c27b23bcd13875e0563ad5e469e
zezeze97 <18428308691@163.com>
2023-08-11 10:33:10 +0800

    update

--- py-bsds500/eval_thebe.sh
+++ py-bsds500/eval_thebe.sh
@@ -2,7 +2,7 @@
 
 PRED_PATH=$1
 
-python evalFault.py --gt_path /home/zhangzr/Fault_Recong/Fault_data/public_data/precessed/test/fault/faulttest.npy \
+python evalFault.py --gt_path /lustre/home/2001110054/Fault_Recong/Fault_data/public_data/precessed/test/fault/faulttest.npy \
                     --pred_path $PRED_PATH \
                     --step 5 \
                     --scalefactor 3 \
