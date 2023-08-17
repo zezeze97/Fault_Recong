f62e93a824b35c8afc97cded28fdc0c0e5dd9fdd
zezeze97 <18428308691@163.com>
2023-08-17 16:09:16 +0800

    update

--- convert_pred.py
+++ convert_pred.py
@@ -1,14 +1,15 @@
-import h5py
 import numpy as np
+import segyio
+import argparse
 
-with h5py.File('/home/zhangzr/FaultRecongnition/pytorch3dunet/3dunet-pdo/Predict-pdo/real/Fault_data_test_predictions.h5','r') as f:
-    data = f['predictions'][:]
-data = data.squeeze(0)
-data[data >= 0.5] = 1
-data[data < 0.5] = 0
-data_flatten = data.flatten()
-data_flatten.tofile('/home/zhangzr/FaultRecongnition/pytorch3dunet/3dunet-pdo/Predict-pdo/real/s4_unet_3d_predict.bin')
-# np.save('/home/zhangzr/FaultRecongnition/pytorch3dunet/3dunet/3DUnet-Predict/real/unet_3d_predict.bin', data)
-# data_read = np.fromfile('/home/zhangzr/FaultRecongnition/pytorch3dunet/3dunet-pdo/Predict-pdo/real/s4_unet_3d_predict.bin', dtype=np.single)
-# data_read = data_read.reshape(101,1201,2751)
-# print(data==data_read)
+def ConvertScoreToSGY(score_path, th, save_path):
+    '''
+    args:
+        - score_path: .npy score file
+        - th: threshold
+        - save_path: path to save converted file
+    
+    '''
+    score = np.load(score_path)
+    prediction = (score > th).astype(np.uint8)
+    # segyio.open(save_path, )
