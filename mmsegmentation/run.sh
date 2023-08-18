14adef73fff00ab4917ffc3ce3de08a8174ef96d
2001110054 <2001110054@bd-data01.hpc.pku.edu.cn>
2023-08-18 16:17:43 +0800

    update

--- mmsegmentation/run.sh
+++ mmsegmentation/run.sh
@@ -1,35 +1,11 @@
+#!/bin/bash
 GPU=$2
 port=23509
 
 
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault-512x512
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_public-512x512
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_public-128x128
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_public_slice-512x512
-# config=swin-base-patch4-window7_upernet_8xb2-16k_fault_real_labeled_mini-128x128_ft
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_real_labeled_slice-256x256_ft
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_public_slice_25d-256x256
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_public_slice_25d-512x512
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_real_labeled_slice_25d-256x256_ft
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_2Dfault_0519_force3chan-256x256
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_2Dfault_0519_slice_split_force3chan-256x256
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_2Dfault_0519_slice_split_force3chan-256x256_ft
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_public_slice_25d-512x512_per_image_normal
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_2Dfault_0519_force3chan-256x256_per_image_norm_pos_weight_10
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_public_slice_25d-512x512_per_image_normal_pos_weight_10
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_2Dfault_0519_slice_split_force3chan-256x256_per_image_norm_pos_weight_10_dilate
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_2Dfault_0519_slice_split_force3chan-256x256_per_image_norm_pos_weight_10_dilate_mim
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_2Dfault_0519_force3chan-256x256_per_image_norm_pos_weight_10_dilate
-# config=swin-base-patch4-window7_upernet_8xb2-160k_fault_2Dfault_0519_force3chan-256x256_per_image_norm_pos_weight_10_mim
-# config=swin-base-patch4-window7_upernet_8xb2-480k_fault_public_slice_3chan_96x96_per_image_normal_pos_weight_10
-# config=swin-base-patch4-window7_upernet_8xb2-160k_mix_data_force_3_chan-512x512_per_image_normal_pos_weight_10
-# config=mae-base_upernet_8xb2-amp-160k_mix_force_3_chan-512x512
-# config=mae-base_upernet_8xb2-amp-160k_mix_force_3_chan-512x512_pretrained
-config=swin-base-patch4-window7_upernet_8xb2-160k_mix_data_force_3_chan-512x512_per_image_normal_simmim_300e
-# config=swin-base-patch4-window7_upernet_8xb2-160k_mix_data_force_3_chan-512x512_per_image_normal
-# config=mae-base_upernet_8xb2-amp-160k_mix_force_3_chan-512x512
+config=swin-base-patch4-window7_upernet_8xb2-160k_mix_data_force_3_chan-512x512_per_image_normal_no_pretrain
 if [ $1 = "train" ]; then
-    CUDA_VISIBLE_DEVICES=$GPU PORT=${port} ./tools/dist_train.sh ./projects/Fault_recong/config/${config}.py 8 --work-dir output/${config} 
+    CUDA_VISIBLE_DEVICES=$GPU PORT=${port} ./tools/dist_train.sh ./projects/Fault_recong/config/${config}.py 8 --work-dir output/${config}
 elif [ $1 = "test" ]; then
     CUDA_VISIBLE_DEVICES=$GPU ./tools/dist_test.sh ./output/swin-base-patch4-window7_upernet_8xb2-160k_fault_public-128x128/swin-base-patch4-window7_upernet_8xb2-160k_fault_public-128x128.py ./output/swin-base-patch4-window7_upernet_8xb2-160k_fault_public-128x128/iter_48000.pth 1
 fi
