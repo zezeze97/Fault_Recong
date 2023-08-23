b1dbb4435695ccf9b9741d576c1ac22b912ffabd
zezeze97 <18428308691@163.com>
2023-08-23 13:46:15 +0800

    update

--- MIM-Med3D/code/models/__init__.py
+++ MIM-Med3D/code/models/__init__.py
@@ -5,3 +5,4 @@ from .swin_unetr import SwinUNETR
 from .swin_unetr_special_ssl import SSLHead, SSLHead_2Task
 from .swin_unetr_multi_scale_decoder import SwinUNETR_Multi_Decoder
 from .swin_unetr_seg_cls import SwinUNETR_Seg_Cls
+from .swin_unetr_multi_scale_decoder_fusion import SwinUNETR_Multi_Decoder_Fusion
