b07bdd046b9b15e8147aa4f4781f88ff63bca9e3
zezeze97 <18428398691@163.com>
2023-11-14 15:14:22 +0800

    111

--- mmsegmentation/run.sh
+++ mmsegmentation/run.sh
@@ -3,6 +3,6 @@ GPU=$1
 port=23509
 
 
-# config=swin-base-simmim
-config=swin-base-simmim-yd-data
+config=swin-base-simmim
+# config=swin-base-simmim-yd-data
 CUDA_VISIBLE_DEVICES=$GPU PORT=${port} python ./tools/train.py ./projects/Fault_recong/config/${config}.py --work-dir output/${config}
