9d59325397bc5d3f99d6925850c10fea61a4a305
zezeze97 <18428308691@163.com>
2023-12-07 14:45:44 +0800

    111

--- MIM-Med3D/slurm_train.sh
+++ MIM-Med3D/slurm_train.sh
@@ -2,10 +2,10 @@
 #SBATCH -o job.%j.out
 #SBATCH --partition=GPU40G
 #SBATCH --qos=high
-#SBATCH -J swin_unetr_base_whole_random_crop_simmim-500e-1x4
-#SBATCH --nodes=1          
+#SBATCH -J swin_unetr_base_whole_random_crop_simmim-500e-4x4
+#SBATCH --nodes=4          
 #SBATCH --cpus-per-task=16   
-#SBATCH --ntasks=4
+#SBATCH --ntasks=16
 #SBATCH --ntasks-per-node=4
 #SBATCH --gres=gpu:4  
 #SBATCH --time=5-00:00:00
